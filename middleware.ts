5ea75291e77b6b19e9985fb622ded1dff08b7ce0
m1000 <74680137+xParzivalx@users.noreply.github.com>
2024-08-28 17:40:51 +0200

    Add files via upload

--- middleware.ts
+++ middleware.ts
@@ -1,15 +1,43 @@
-import createMiddleware from 'next-intl/middleware';
-
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+import { NextResponse } from 'next/server';
+import createIntlMiddleware from 'next-intl/middleware';
 import { languageId } from '@/config/i18n';
 
-export default createMiddleware({
-    locales: languageId,
+const intlMiddleware = createIntlMiddleware({
+  locales: languageId,
+  defaultLocale: 'es',
+  localePrefix: 'never',
+});
 
-    defaultLocale: 'en',
+const isPublicRoute = createRouteMatcher(['/sign-in(.*)']);
+const isApiRoute = createRouteMatcher(['/api/chat(.*)', '/api/search(.*)', '/api/app(.*)']);
 
-    localePrefix: 'never',
+export default clerkMiddleware((auth, req) => {
+  if (isApiRoute(req)) {
+    // Permite que las rutas API se procesen sin interferencia del middleware de Clerk
+    return NextResponse.next();
+  }
+
+  if (!isPublicRoute(req)) {
+    const { userId } = auth();
+    if (!userId) {
+      const signInUrl = new URL('https://legible-ox-61.accounts.dev/sign-in', req.url);
+      signInUrl.searchParams.set('redirect_url', req.url);
+      return NextResponse.redirect(signInUrl);
+    }
+  }
+
+  return intlMiddleware(req);
 });
 
 export const config = {
-    matcher: ['/', '/search', '/(de|en|es|fr|it|ja|ko|nl|pt|ru|zh-CN|zh-HK|zh-TW)/:path*'],
+  matcher: [
+    '/((?!.*\\..*|_next).*)',
+    '/',
+    '/(api|trpc)(.*)', // Esto manejará las rutas /api/ y /trpc/
+    '/(de|en|es|fr|it|ja|ko|nl|pt|ru|zh-CN|zh-HK|zh-TW)/:path*', // Maneja la internacionalización
+    // Asegúrate de que las rutas de API específicas también estén incluidas
+    '/api/chat/:path*',
+    '/api/search/:path*',
+  ],
 };
