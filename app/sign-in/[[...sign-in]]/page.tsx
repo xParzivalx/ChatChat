a86123d19552d5d59d44da1a9c2f19d53e908891
m1000 <74680137+xParzivalx@users.noreply.github.com>
2024-09-06 00:18:44 +0200

    a

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -77,6 +77,7 @@ export default function SignInPage() {
           path="/sign-in"
           routing="path"
           appearance={{
+            elements: {
               footer: { display: "none" },
             },
           }}
