f6cf0057e0b9cd51e34ba13679a75d01a51a6c65
m1000 <74680137+xParzivalx@users.noreply.github.com>
2024-09-06 16:32:28 +0200

    sgfdv

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import { ClerkProvider } from '@clerk/nextjs';
 import { dark } from '@clerk/themes';
+import { esES } from "@clerk/localizations";
 import type { Metadata, Viewport } from 'next';
 import { Onest } from 'next/font/google';
 
@@ -37,7 +38,9 @@ export default function RootLayout({
 }>) {
     return (
         <ClerkProvider 
+        
         appearance={{
+            localization={esES},
             baseTheme: dark,
             elements: {
               footer: "hidden",
