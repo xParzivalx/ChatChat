07d2adf92e58907da7366e82365c32ca6f6634fb
m1000 <74680137+xParzivalx@users.noreply.github.com>
2024-09-06 15:43:00 +0200

    sdfsdf

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,13 @@
 import '@/styles/globals.css';
 import 'tippy.js/dist/tippy.css';
 
-import RootProvider from '@/app/provider';
-import type { Metadata, Viewport } from 'next';
-import { Onest } from 'next/font/google';
 import { ClerkProvider } from '@clerk/nextjs';
 import { esES } from "@clerk/localizations";
 import { dark } from '@clerk/themes';
+import type { Metadata, Viewport } from 'next';
+import { Onest } from 'next/font/google';
 
-
-
+import RootProvider from './provider';
 
 const onest = Onest({ subsets: ['latin'] });
 
